6837b11f2a26da4b226928b96b5872a56945ad74
Gonzalo Fonseca <fonsecagonzalo190@gmail.com>
2024-02-06 11:15:38 -0300

    anotaciones hasta ejercicio 5

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_05.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_05.py
@@ -5,8 +5,9 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Gonzalo
+apellido: Fonseca
+Tutor: Alejandro
 ---
 Ejercicio: entrada_salida_05
 ---
